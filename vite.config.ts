001f0e907d1f987b21b63a9140fbc933e7467cbe
ZpacmanF <joaocarvalho.2002@alunos.utfpr.edu.br>
2025-01-13 16:34:28 -0300

    build: adjustment deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react_patents_search_app/",
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
